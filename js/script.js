f855a18c6c179ddde3a984a473f45e56f1971d1e
12cynthia12 <cynthia.luethi@stud.fhgr.ch>
2026-06-01 15:59:03 +0200

    leaflet karte

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("blub");
+
+// karte erstellen, keinen zoom erlauben, keine interaktionen erlauben
+const map = L.map('map', {
+    minZoom: 2, maxZoom: 2, zoomControl: false, 
+    dragging: false, scrollWheelZoom: false, doubleClickZoom: false, 
+}).setView([50, 0],2);
+// karte laden, macht dass karte nicht unendlihc ist, sondern nur einmal gezeigt wird
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    noWrap: true,
+    bounds: [[-90,-180], [90,180]]
+}).addTo(map)
